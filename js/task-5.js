4653dde9416dededb3df60a6135ce4ae5cf0b856
Voroshylo <152655013+Voroshylo@users.noreply.github.com>
2024-03-29 21:32:27 +0200

    1

--- js/task-5.js
+++
@@ -1,9 +0,0 @@
-function getSlice(array, value) {
-    const index = array.indexOf(value);
-    if (index === -1) {
-        return [];
-    } else {
-        return array.slice(0, index + 1);
-    }
-}
-console.log(getSlice(["Mango", "Poly", "Ajax"], "Poly"));
